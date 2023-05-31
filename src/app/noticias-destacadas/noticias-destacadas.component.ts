f967d9f40cded84586bb3541223eefd78f0d271e
Fernando Gaspari <dev.gaspari.fernando@gmail.com>
2023-05-31 14:16:05 -0300

    creacion del componente noticias-destacadas

---
+++ src/app/noticias-destacadas/noticias-destacadas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias-destacadas',
+  templateUrl: './noticias-destacadas.component.html',
+  styleUrls: ['./noticias-destacadas.component.scss']
+})
+export class NoticiasDestacadasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
